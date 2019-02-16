31c2d445e1abcca2e397f2e840d774e2d15c5353
Vladimir Zarypov <krre31@gmail.com>
2019-02-16 07:31:25 +0200

    Add context menu to outliner

--- src/ui/Outliner.cpp
+++ src/ui/Outliner.cpp
@@ -1,5 +1,27 @@
 #include "Outliner.h"
+#include <QtWidgets>
 
 Outliner::Outliner() {
+    setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(this, &QTreeView::customContextMenuRequested, this, &Outliner::onCustomContextMenu);
+    createContextMenu();
+}
 
+void Outliner::onCustomContextMenu(const QPoint& point) {
+    if (indexAt(point).isValid()) {
+        contextMenu->exec(mapToGlobal(point));
+    } else {
+        contextMenu->exec(mapToGlobal(point));
+    }
+}
+
+void Outliner::addNote() {
+    qDebug() << "add note";
+}
+
+void Outliner::createContextMenu() {
+    contextMenu = new QMenu(this);
+
+    QAction* addAction = contextMenu->addAction(tr("Add"));
+    connect(addAction, &QAction::triggered, this, &Outliner::addNote);
 }
--- src/ui/Outliner.h
+++ src/ui/Outliner.h
@@ -1,9 +1,20 @@
 #pragma once
 #include <QTreeView>
 
+class QMenu;
+
 class Outliner : public QTreeView {
     Q_OBJECT
 
 public:
     Outliner();
+
+private slots:
+    void onCustomContextMenu(const QPoint& point);
+    void addNote();
+
+private:
+    void createContextMenu();
+
+    QMenu* contextMenu;
 };
