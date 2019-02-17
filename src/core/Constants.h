6cdddf05544fa3a6bc518aecfe0ef43e3dc6605b
Vladimir Zarypov <krre31@gmail.com>
2019-02-17 16:19:25 +0200

    Replace const by constexpr

--- src/core/Constants.h
+++ src/core/Constants.h
@@ -2,12 +2,12 @@
 
 namespace Constants {
     namespace App {
-        const char ORGANIZATIION[] = "Memo";
-        const char NAME[] = "Memo";
-        const char VERSION[] = "0.1.0";
-        const char STATUS[] = "Preview Alpha";
-        const char URL[] = "https://github.com/krre/memo";
-        const char SETTINGS_NAME[] = "memo.conf";
-        const char COPYRIGHT[] = "Copyright © 2019, Vladimir Zarypov";
+        constexpr auto ORGANIZATIION = "Memo";
+        constexpr auto NAME = "Memo";
+        constexpr auto VERSION = "0.1.0";
+        constexpr auto STATUS = "Preview Alpha";
+        constexpr auto URL = "https://github.com/krre/memo";
+        constexpr auto SETTINGS_NAME = "memo.conf";
+        constexpr auto COPYRIGHT = "Copyright © 2019, Vladimir Zarypov";
     }
 }
