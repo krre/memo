010a96d646b8baef0003b26ba198085e73e6e156
Vladimir Zarypov <krre31@gmail.com>
2019-02-23 11:02:30 +0200

    Release 1.0

--- src/core/Constants.h
+++ src/core/Constants.h
@@ -4,8 +4,8 @@ namespace Constants {
     namespace App {
         constexpr auto Organization = "Memo";
         constexpr auto Name = "Memo";
-        constexpr auto Version = "0.1.0";
-        constexpr auto Status = "Preview Alpha";
+        constexpr auto Version = "1.0";
+        constexpr auto Status = ""; // Release, Beta, Alpha
         constexpr auto URL = "https://github.com/krre/memo";
         constexpr auto SettingsName = "memo.conf";
         constexpr auto Copyright = "Copyright © 2019, Vladimir Zarypov";
