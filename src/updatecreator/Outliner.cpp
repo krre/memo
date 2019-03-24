877fe95acc15df03a8e70d25f35886b9f99d0ccb
Vladimir Zarypov <krre31@gmail.com>
2019-03-24 07:42:58 +0200

    UpdateCreator: Add widgets to Outliner

--- src/updatecreator/Outliner.cpp
+++ src/updatecreator/Outliner.cpp
@@ -1,5 +1,36 @@
 #include "Outliner.h"
+#include <QtWidgets>
 
 Outliner::Outliner(QWidget* parent) : QWidget(parent) {
     setMinimumWidth(100);
+
+    auto layout = new QVBoxLayout;
+    layout->setMargin(0);
+    layout->setSpacing(0);
+    setLayout(layout);
+
+    listWidget = new QListWidget;
+    layout->addWidget(listWidget);
+
+    auto addButton = new QPushButton(tr("Add"));
+    connect(addButton, &QPushButton::clicked, this, &Outliner::addUpdate);
+
+    auto removeButton = new QPushButton(tr("Remove"));
+    connect(removeButton, &QPushButton::clicked, this, &Outliner::removeUpdate);
+
+    auto buttonLaoyut = new QHBoxLayout;
+    buttonLaoyut->setMargin(5);
+    buttonLaoyut->setSpacing(5);
+    buttonLaoyut->addWidget(addButton);
+    buttonLaoyut->addWidget(removeButton);
+
+    layout->addLayout(buttonLaoyut);
+}
+
+void Outliner::addUpdate() {
+    qDebug() << "add";
+}
+
+void Outliner::removeUpdate() {
+    qDebug() << "remove";
 }
--- src/updatecreator/Outliner.h
+++ src/updatecreator/Outliner.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <QWidget>
 
+class QListWidget;
+
 class Outliner : public QWidget {
     Q_OBJECT
 public:
     explicit Outliner(QWidget* parent = nullptr);
+
+private slots:
+    void addUpdate();
+    void removeUpdate();
+
+private:
+    QListWidget* listWidget = nullptr;
 };
