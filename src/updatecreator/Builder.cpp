aee49adb0c2ded700d57a807386c28e8b1676337
Vladimir Zarypov <krre31@gmail.com>
2019-03-30 20:03:26 +0200

    UpdateCreator: Add application selector to Builder tab

--- src/updatecreator/Builder.cpp
+++ src/updatecreator/Builder.cpp
@@ -5,6 +5,7 @@ Builder::Builder(QWidget* parent) : QWidget(parent) {
     auto layout = new QVBoxLayout;
     setLayout(layout);
 
+    createAppDirWidgets();
     createFilesWidgets();
 
     auto buildButton = new QPushButton(tr("Build"));
@@ -15,6 +16,27 @@ Builder::Builder(QWidget* parent) : QWidget(parent) {
 
 Builder::~Builder() {
     writeSettings();
+}
+
+void Builder::selectDirectory() {
+    QString directory = QFileDialog::getExistingDirectory(this);
+
+    if (!directory.isEmpty()) {
+        appDirLineEdit->setText(directory);
+    }
+}
+
+void Builder::createAppDirWidgets() {
+    auto appDirLayout = new QHBoxLayout;
+    appDirLayout->addWidget(new QLabel(tr("Application:")));
+    appDirLineEdit = new QLineEdit;
+    appDirLayout->addWidget(appDirLineEdit);
+    auto appDirButton = new QPushButton(tr("Browse..."));
+    connect(appDirButton, &QPushButton::clicked, this, &Builder::selectDirectory);
+    appDirLayout->addWidget(appDirButton);
+
+    auto mainLayout = static_cast<QVBoxLayout*>(layout());
+    mainLayout->addLayout(appDirLayout);
 }
 
 void Builder::createFilesWidgets() {
--- src/updatecreator/Builder.h
+++ src/updatecreator/Builder.h
@@ -9,9 +9,15 @@ public:
     explicit Builder(QWidget* parent = nullptr);
     ~Builder();
 
+private slots:
+    void selectDirectory();
+
 private:
+    void createAppDirWidgets();
     void createFilesWidgets();
 
     void readSettings();
     void writeSettings();
+
+    QLineEdit* appDirLineEdit = nullptr;
 };
