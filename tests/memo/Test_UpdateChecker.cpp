6c2c4291fe38606251e01d0a4107a4a0f856a585
Vladimir Zarypov <krre31@gmail.com>
2019-05-25 17:30:40 +0300

    tests: Add real test for UpdateChecker

--- tests/memo/Test_UpdateChecker.cpp
+++ tests/memo/Test_UpdateChecker.cpp
@@ -1,14 +1,34 @@
 #include <QtTest>
-#include <database/Database.h>
+#include <core/Context.h>
+#include <updater/UpdateChecker.h>
 
 class Test_UpdateChecker : public QObject {
     Q_OBJECT
+
 private slots:
-    void checkDatabase();
+    void initTestCase();
+    void check();
+
+private:
+    Context* context = nullptr;
 };
 
-void Test_UpdateChecker::checkDatabase() {
-    Database database;
+void Test_UpdateChecker::initTestCase() {
+    context = new Context(this);
+}
+
+void Test_UpdateChecker::check() {
+    UpdateChecker updateChecker;
+    qRegisterMetaType<QVector<UpdateChecker::Update>>("QVector<UpdateChecker::Update>");
+
+    QSignalSpy spy(&updateChecker, &UpdateChecker::checkResult);
+    updateChecker.check();
+
+    QVERIFY(spy.wait());
+    QCOMPARE(spy.count(), 1);
+
+    auto updates = qvariant_cast<QVector<UpdateChecker::Update>>(spy.takeFirst());
+    qDebug() << updates.size();
 }
 
 QTEST_MAIN(Test_UpdateChecker)
