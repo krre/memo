1d94d9ea012961123a00e3086bdb1c8c4ea22f8f
Vladimir Zarypov <krre31@gmail.com>
2022-11-02 18:27:02 +0200

    Update translation

--- src/i18n/memo-ru.ts
+++ src/i18n/memo-ru.ts
@@ -4,187 +4,102 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="136"/>
+        <location filename="../ui/MainWindow.cpp" line="125"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="137"/>
+        <location filename="../ui/MainWindow.cpp" line="126"/>
         <source>New...</source>
         <translation>Создать...</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="138"/>
+        <location filename="../ui/MainWindow.cpp" line="127"/>
         <source>Open...</source>
         <translation>Открыть...</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="146"/>
+        <location filename="../ui/MainWindow.cpp" line="135"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="140"/>
+        <location filename="../ui/MainWindow.cpp" line="129"/>
         <source>Recent Files</source>
         <translation>Недавние файлы</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="142"/>
+        <location filename="../ui/MainWindow.cpp" line="131"/>
         <source>Clear</source>
         <translation>Очистить</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="145"/>
+        <location filename="../ui/MainWindow.cpp" line="134"/>
         <source>Export All...</source>
         <translation>Экспортировать всё...</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="149"/>
-        <location filename="../ui/MainWindow.cpp" line="174"/>
+        <location filename="../ui/MainWindow.cpp" line="138"/>
+        <source>Preferences...</source>
+        <translation>Настройки...</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.cpp" line="140"/>
+        <location filename="../ui/MainWindow.cpp" line="155"/>
         <source>Hide</source>
         <translation>Спрятать</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="151"/>
-        <location filename="../ui/MainWindow.cpp" line="176"/>
+        <location filename="../ui/MainWindow.cpp" line="142"/>
+        <location filename="../ui/MainWindow.cpp" line="157"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="153"/>
-        <source>Tools</source>
-        <translation>Инструменты</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="154"/>
-        <source>Options...</source>
-        <translation>Настройки...</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="162"/>
+        <location filename="../ui/MainWindow.cpp" line="144"/>
         <source>Help</source>
         <translation>Справка</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="163"/>
-        <source>Check for updates...</source>
-        <translation>Проверить обновления...</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="164"/>
+        <location filename="../ui/MainWindow.cpp" line="145"/>
         <source>Open download page</source>
         <translation>Открыть страницу загрузки</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="167"/>
+        <location filename="../ui/MainWindow.cpp" line="148"/>
         <source>About %1...</source>
         <translation>О программе %1...</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="173"/>
+        <location filename="../ui/MainWindow.cpp" line="154"/>
         <source>Show</source>
         <translation>Показать</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="242"/>
+        <location filename="../ui/MainWindow.cpp" line="224"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <source>New File</source>
-        <translation type="vanished">Новый файл</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="252"/>
-        <location filename="../ui/MainWindow.cpp" line="274"/>
+        <location filename="../ui/MainWindow.cpp" line="234"/>
+        <location filename="../ui/MainWindow.cpp" line="256"/>
         <source>All Files (*);;Database Files (*.db)</source>
         <translation>Все файлы (*);;Файлы баз данных (*.db)</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="260"/>
+        <location filename="../ui/MainWindow.cpp" line="242"/>
         <source>Error rewriting old file</source>
         <translation>Ошибка перезаписи старого файла</translation>
     </message>
     <message>
-        <source>Open File</source>
-        <translation type="vanished">Открыть файл</translation>
-    </message>
-    <message>
-        <source>Select Directory</source>
-        <translation type="vanished">Выбрать директорию</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="306"/>
-        <source>Check of updates</source>
-        <translation>Проверка обновлений</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="306"/>
-        <source>Latest version of %1 is installed</source>
-        <translation>Установлена последняя версия %1</translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.cpp" line="325"/>
+        <location filename="../ui/MainWindow.cpp" line="297"/>
         <source>About %1</source>
         <translation>О программе %1</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.cpp" line="326"/>
+        <location filename="../ui/MainWindow.cpp" line="298"/>
         <source>&lt;h3&gt;%1 %2 %3&lt;/h3&gt;           Outliner for quick notes&lt;br&gt;&lt;br&gt;            Based on Qt %4&lt;br&gt;            Build on %5 %6&lt;br&gt;&lt;br&gt;            &lt;a href=%7&gt;%7&lt;/a&gt;&lt;br&gt;&lt;br&gt;Copyright © %8, Vladimir Zarypov</source>
         <translation>&lt;h3&gt;%1 %2 %3&lt;/h3&gt;           Менеджер быстрых заметок&lt;br&gt;&lt;br&gt;            Основано на Qt %4&lt;br&gt;            Собрано %5 %6&lt;br&gt;&lt;br&gt;            &lt;a href=%7&gt;%7&lt;/a&gt;&lt;br&gt;&lt;br&gt;© %8, Владимир Зарыпов</translation>
-    </message>
-</context>
-<context>
-    <name>NewUpdates</name>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="8"/>
-        <source>New Updates Available</source>
-        <translation>Доступны новые обновления</translation>
-    </message>
-    <message>
-        <source>Version: %1 - Date: %2 - Size: %3.
-</source>
-        <translation type="vanished">Версия: %1 - Дата: %2 - Размер: %3.
-</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="14"/>
-        <source>Version: %1 (%2) - Date: %3
-</source>
-        <translation>Версия: %1 (%2) - Дата: %3
-</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="33"/>
-        <source>Channel:</source>
-        <translation>Канал:</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="36"/>
-        <source>Count:</source>
-        <translation>Количество:</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="39"/>
-        <source>Size:</source>
-        <translation>Размер:</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="45"/>
-        <source>Updating to this version is impossible!
-Download installation of application by link below, reinstall it and try update again.</source>
-        <translation>Обновление до этой версии невозможно!
-Загрузите инсталляцию по ссылке ниже, переустановите приложение и попробуйте обновить ещё раз.</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="93"/>
-        <source>MB</source>
-        <translation>МБ</translation>
-    </message>
-    <message>
-        <location filename="../updater/NewUpdates.cpp" line="58"/>
-        <source>Update</source>
-        <translation>Обновить</translation>
     </message>
 </context>
 <context>
@@ -208,84 +123,6 @@ Download installation of application by link below, reinstall it and try update 
         <location filename="../ui/outliner/NoteProperties.cpp" line="19"/>
         <source>Updated at:</source>
         <translation>Обновлено:</translation>
-    </message>
-</context>
-<context>
-    <name>Options</name>
-    <message>
-        <location filename="../ui/Options.cpp" line="7"/>
-        <source>Options</source>
-        <translation>Настройки</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="10"/>
-        <source>User Interface</source>
-        <translation>Интерфейс пользователя</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="16"/>
-        <source>Language:</source>
-        <translation>Язык:</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="19"/>
-        <source>&lt;System&gt;</source>
-        <translation>&lt;Системный&gt;</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="26"/>
-        <source>Font:</source>
-        <translation>Шрифт:</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="38"/>
-        <source>Open...</source>
-        <translation>Открыть...</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="45"/>
-        <source>Minimize to tray on startup</source>
-        <translation>Минимизировать в трей при запуске</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="49"/>
-        <source>Hide tray icon</source>
-        <translation>Спрятать иконку трея</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="57"/>
-        <source>Global Hotkey</source>
-        <translation>Глобальная горячая клавиша</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="67"/>
-        <source>Updates</source>
-        <translation>Обновления</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="72"/>
-        <source>Channel:</source>
-        <translation>Канал:</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="78"/>
-        <source>Check on startup</source>
-        <translation>Проверять при запуске приложения</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="100"/>
-        <source>Restart requred</source>
-        <translation>Требуется перезапуск</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="100"/>
-        <source>You must restart application</source>
-        <translation>Вы должны перезапустить приложение</translation>
-    </message>
-    <message>
-        <location filename="../ui/Options.cpp" line="112"/>
-        <source>Select Font</source>
-        <translation>Выбрать шрифт</translation>
     </message>
 </context>
 <context>
@@ -352,14 +189,77 @@ Download installation of application by link below, reinstall it and try update 
     </message>
 </context>
 <context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="6"/>
+        <source>Preferences</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="9"/>
+        <source>User Interface</source>
+        <translation>Интерфейс пользователя</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="15"/>
+        <source>Language:</source>
+        <translation>Язык:</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="18"/>
+        <source>&lt;System&gt;</source>
+        <translation>&lt;Системный&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="25"/>
+        <source>Font:</source>
+        <translation>Шрифт:</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="37"/>
+        <source>Open...</source>
+        <translation>Открыть...</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="44"/>
+        <source>Minimize to tray on startup</source>
+        <translation>Минимизировать в трей при запуске</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="48"/>
+        <source>Hide tray icon</source>
+        <translation>Спрятать иконку трея</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="56"/>
+        <source>Global Hotkey</source>
+        <translation>Глобальная горячая клавиша</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="81"/>
+        <source>Restart requred</source>
+        <translation>Требуется перезапуск</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="81"/>
+        <source>You must restart application</source>
+        <translation>Вы должны перезапустить приложение</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.cpp" line="93"/>
+        <source>Select Font</source>
+        <translation>Выбрать шрифт</translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="19"/>
+        <location filename="../main.cpp" line="41"/>
         <source>Systray</source>
         <translation>Системный трей</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="20"/>
+        <location filename="../main.cpp" line="42"/>
         <source>Absent any system tray on this system</source>
         <translation>На этой системе отсутствует системный трей</translation>
     </message>
