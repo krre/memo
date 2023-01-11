6b297c846124f7f306b0ea290fb5fa02b4115bc0
Vladimir Zarypov <krre31@gmail.com>
2023-01-11 07:44:07 +0200

    Fix Clazy warning temporary-iterator

--- src/server/handler/Handler.cpp
+++ src/server/handler/Handler.cpp
@@ -8,8 +8,9 @@ Handler::Handler(Database* database) : m_database(database) {
 
 QHttpServerResponse Handler::exec(const QHttpServerRequest& request, const QString& key) {
     bool accessOk = false;
+    auto headers = request.headers();
 
-    for (auto it = request.headers().cbegin(); it != request.headers().cend(); it++) {
+    for (auto it = headers.cbegin(); it != headers.cend(); it++) {
         if (it->first.toLower() == "token" && it->second == key) {
             accessOk = true;
             break;
