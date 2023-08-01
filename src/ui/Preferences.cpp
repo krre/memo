b6ee71fa473195e6d4c0d63b894c51e444d3e8ac
Vladimir Zarypov <krre31@gmail.com>
2023-08-01 19:01:27 +0300

    Improve setup aligment of comboboxes

--- src/ui/Preferences.cpp
+++ src/ui/Preferences.cpp
@@ -132,8 +132,9 @@ QGroupBox* Preferences::createUiGroupBox(const Data& data) {
 
     auto formLayout = new QFormLayout;
     formLayout->addRow(tr("Language:"), m_languageComboBox);
-    formLayout->itemAt(0, QFormLayout::FieldRole)->setAlignment(Qt::AlignLeft);
     formLayout->addRow(tr("Font:"), fontLayout);
+
+    formLayout->itemAt(formLayout->indexOf(m_languageComboBox))->setAlignment(Qt::AlignLeft);
 
     auto result = new QGroupBox(tr("User Interface"));
     auto verticalLayout = new QVBoxLayout(result);
