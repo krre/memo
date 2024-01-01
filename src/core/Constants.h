ed1555b3810aea941ca40dffbf7f5f3d34748eda
Vladimir Zarypov <krre31@gmail.com>
2024-01-01 06:58:36 +0200

    Release 1.8.0

--- src/core/Constants.h
+++ src/core/Constants.h
@@ -5,7 +5,7 @@ namespace Const {
 namespace App {
     constexpr auto Organization = "Memo";
     constexpr auto Name = "Memo";
-    constexpr auto Version = "1.7.1";
+    constexpr auto Version = "1.8.0";
     constexpr auto Status = ""; // Release, Beta, Alpha
     constexpr auto URL = "https://github.com/krre/memo-desktop";
     constexpr auto ReleasesUrl = "https://github.com/krre/memo-desktop/releases";
