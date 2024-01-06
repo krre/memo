bf00eb95b8452a31bb5b89abcf9b8330efd03a81
Vladimir Zarypov <krre31@gmail.com>
2024-01-06 07:39:34 +0200

    Fix code format

--- src/ui/notetaking/TreeModel.h
+++ src/ui/notetaking/TreeModel.h
@@ -15,7 +15,7 @@ public:
     int rowCount(const QModelIndex& parent) const override;
     int columnCount(const QModelIndex& parent) const override;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
     bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
 
     Qt::DropActions supportedDropActions() const override;
