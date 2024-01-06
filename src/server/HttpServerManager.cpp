e435e747c0987a17cbd6d60c602c4eee4130d937
Vladimir Zarypov <krre31@gmail.com>
2024-01-06 08:23:04 +0200

    Cleanup includes

--- src/server/HttpServerManager.cpp
+++ src/server/HttpServerManager.cpp
@@ -4,9 +4,6 @@
 #include "handler/NameHandler.h"
 #include "handler/NotesHandler.h"
 #include <QHttpServer>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonArray>
 #include <QFile>
 #include <QSslKey>
 
