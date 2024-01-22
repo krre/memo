a70490b72e39df56cff2bc83d50bf073868e4add
Vladimir Zarypov <krre31@gmail.com>
2024-01-22 08:45:04 +0200

    Refactor message handler

--- src/core/MessageHandler.h
+++ src/core/MessageHandler.h
@@ -2,22 +2,23 @@
 #include <QString>
 
 void messageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg) {
-     QByteArray localMsg = msg.toLocal8Bit();
+     auto localMsg = msg.toLocal8Bit().constData();
+
      switch (type) {
-     case QtDebugMsg:
-         fprintf(stderr, "[Debug] %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-         break;
-     case QtInfoMsg:
-         fprintf(stderr, "[Info] %s\n", localMsg.constData());
-         break;
-     case QtWarningMsg:
-         fprintf(stderr, "[Warning] %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-         break;
-     case QtCriticalMsg:
-         fprintf(stderr, "[Critical] %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-         break;
-     case QtFatalMsg:
-         fprintf(stderr, "[Fatal] %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-         break;
+         case QtDebugMsg:
+             fprintf(stderr, "[Debug] %s (%s:%u, %s)\n", localMsg, context.file, context.line, context.function);
+             break;
+         case QtInfoMsg:
+             fprintf(stderr, "[Info] %s\n", localMsg);
+             break;
+         case QtWarningMsg:
+             fprintf(stderr, "[Warning] %s (%s:%u, %s)\n", localMsg, context.file, context.line, context.function);
+             break;
+         case QtCriticalMsg:
+             fprintf(stderr, "[Critical] %s (%s:%u, %s)\n", localMsg, context.file, context.line, context.function);
+             break;
+         case QtFatalMsg:
+             fprintf(stderr, "[Fatal] %s (%s:%u, %s)\n", localMsg, context.file, context.line, context.function);
+             break;
     }
 }
