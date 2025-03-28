9a8b99231d21bd1658bc666d16e409db43cfe332
Vladimir Zarypov <krre31@gmail.com>
2025-03-28 09:39:49 +0200

    Add Id::toJson() method

--- src/core/Id.cpp
+++ src/core/Id.cpp
@@ -7,3 +7,7 @@ Id::Id(quint64 id) : m_id(id) {
 quint64 Id::value() const {
     return m_id;
 }
+
+QJsonValue Id::toJson() const {
+    return QJsonValue(static_cast<qint64>(m_id));
+}
--- src/core/Id.h
+++ src/core/Id.h
@@ -1,11 +1,12 @@
 #pragma once
-#include <QVector>
+#include <QJsonValue>
 
 class Id {
 public:
     Id(quint64 id);
 
     quint64 value() const;
+    QJsonValue toJson() const;
 
 private:
     quint64 m_id = 0;
