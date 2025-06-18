b169e0266488a907393b7993c6f3e4cb40c9ceec
Vladimir Zarypov <krre31@gmail.com>
2025-06-18 18:39:45 +0300

    Fix migration date

--- src/database/Migrater.h
+++ src/database/Migrater.h
@@ -12,7 +12,7 @@ private:
     void migration1() const; // 16.02.2019
     void migration2() const; // 14.12.2019
     void migration3() const; // 09.12.2023
-    void migration4() const; // 24.10.2023
+    void migration4() const; // 24.10.2024
 
     Database* m_db = nullptr;
     QHash<int, std::function<void()>> migrations;
