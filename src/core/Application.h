5974be18fa9b9be4623ee25ee1b3a56ebc7dc068
Vladimir Zarypov <krre31@gmail.com>
2026-09-19 16:18:40 +0300

    Remove duplication of URL constants

--- src/core/Application.h
+++ src/core/Application.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "src/config.h"
 #include <QApplication>
+#include <QString>
 
 class Application : public QApplication {
 public:
     static constexpr auto Organization = "Memo";
     static constexpr auto Name = "Memo";
     static constexpr auto Version = PROJECT_VERSION;
-    static constexpr auto Url = "https://github.com/krre/memo";
-    static constexpr auto ReleasesUrl = "https://github.com/krre/memo/releases";
+    inline static const auto Url = QStringLiteral("https://github.com/krre/memo");
+    inline static const auto ReleasesUrl = Url + QStringLiteral("/releases");
     static constexpr auto Years = "2019-2026";
     static constexpr auto BuildDate = __DATE__;
     static constexpr auto BuildTime = __TIME__;
